1e203aa1b6b869c83c0c28fe4105dbf127a4c713
neeraj0631 <136948183+neeraj0631@users.noreply.github.com>
2023-08-09 16:56:09 +0530

    Update MODULE 3.sol

--- MODULE 3.sol
+++ MODULE 3.sol
@@ -1,1 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract MyToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+
+    address public owner;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Burn(address indexed from, uint256 value);
+    event Mint(address indexed to, uint256 value);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals,
+        uint256 _initialSupply
+    ) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _initialSupply * 10**uint256(decimals);
+        balanceOf[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+
+    function transfer(address _to, uint256 _value) public {
+        require(_to != address(0), "Invalid recipient address");
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+    }
+
+    function burn(uint256 _value) public {
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+
+        balanceOf[msg.sender] -= _value;
+        totalSupply -= _value;
+
+        emit Burn(msg.sender, _value);
+        emit Transfer(msg.sender, address(0), _value);
+    }
+
+    function mint(address _to, uint256 _value) public onlyOwner {
+        require(_to != address(0), "Invalid recipient address");
+
+        balanceOf[_to] += _value;
+        totalSupply += _value;
+
+        emit Mint(_to, _value);
+        emit Transfer(address(0), _to, _value);
+    }
+}
